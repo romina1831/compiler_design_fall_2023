cf9bcb6c466e5b1dc7efac4ec2ca788fc44bb05e
romina1831 <romina.aghili.rgh@gmail.com>
2023-12-16 16:36:16 +0330

    my first try

---
+++ HW3/gen/main.g4
@@ -0,0 +1,15 @@
+grammar main;
+
+// Define the main rule
+start:  email  website_address  decimal_number national_number EOF;
+
+// Define tokens
+email: EMAIL;
+website_address: WEBSITE_ADDRESS;
+decimal_number: DECIMAL_NUMBER;
+national_number: NATIONAL_NUMBER;
+
+EMAIL: [a-zA-Z0-9]+ '@gmail.com';
+WEBSITE_ADDRESS: 'http://'[a-zA-Z0-9]+'.'[a-zA-Z]+;
+DECIMAL_NUMBER: [0-9]+'.'[0-9]+;
+NATIONAL_NUMBER: [0-9]+;
---
+++ HW3/gen/mainVisitor.py
@@ -0,0 +1,73 @@
+# Generated from C:/Users/Main user/Desktop/compiler_design_fall_2023/HW3/gen/main.g4 by ANTLR 4.13.1
+from antlr4 import *
+if "." in __name__:
+    from .mainParser import mainParser
+else:
+    from mainParser import mainParser
+
+# This class defines a complete generic visitor for a parse tree produced by mainParser.
+
+class mainVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by mainParser#equation.
+    def visitEquation(self, ctx:mainParser.EquationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#expression.
+    def visitExpression(self, ctx:mainParser.ExpressionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#multiplyingExpression.
+    def visitMultiplyingExpression(self, ctx:mainParser.MultiplyingExpressionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#powExpression.
+    def visitPowExpression(self, ctx:mainParser.PowExpressionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#signedAtom.
+    def visitSignedAtom(self, ctx:mainParser.SignedAtomContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#atom.
+    def visitAtom(self, ctx:mainParser.AtomContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#scientific.
+    def visitScientific(self, ctx:mainParser.ScientificContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#constant.
+    def visitConstant(self, ctx:mainParser.ConstantContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#variable.
+    def visitVariable(self, ctx:mainParser.VariableContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#func_.
+    def visitFunc_(self, ctx:mainParser.Func_Context):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#funcname.
+    def visitFuncname(self, ctx:mainParser.FuncnameContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by mainParser#relop.
+    def visitRelop(self, ctx:mainParser.RelopContext):
+        return self.visitChildren(ctx)
+
+
+
+del mainParser
